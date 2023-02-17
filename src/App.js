b61c67b6b2345ca8bd10e6a2bbf9469f839b3b02
nasir1196 <nasiruddin.cf@gmail.com>
2023-02-17 11:15:46 -0500

    added img and width

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import bg from './source/air-bg.jpg';
 function App ()
 {
   return (
     <div className="App">
+      <img style={ { height: "100vh", width: "100%" } } src={ bg } alt="" />
 
     </div>
   );
