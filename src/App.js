63325fe81d03849f36f02a2caed0706c0eb6c87d
nasir1196 <nasiruddin.cf@gmail.com>
2023-04-16 18:06:09 +0600

    navigation and spinner and animation added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import bg from './source/air-bg.jpg';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Flight from './components/screens/Flight';
+import Car from './components/screens/Car';
+import Vacation from './components/screens/Vacation';
+import Package from './components/screens/Package';
+import Home from './components/Home/Home';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import plane from "./source/plane.png";
+
+
+
 function App ()
 {
+
+  const [ loading, setLoading ] = useState( false );
+
+  useEffect( () =>
+  {
+    setLoading( true );
+    setTimeout( () =>
+    {
+      setLoading( false );
+    }, 7500 );
+  }, [] );
+  const date = new Date();
+  const getYear = date.getFullYear();
+
   return (
-    <div className="App">
-
-
-    </div>
+    <BrowserRouter>
+      {
+        loading ? ( <div className="loader">
+          <div className='sky'>
+            <img src={ plane } alt="" />
+          </div>
+        </div> ) : (
+          <>
+            <div className="main-body">
+              <Navbar />
+              <Routes>
+                <Route exact path="/" element={ <Home /> } />
+                <Route exact path="/flight" element={ <Flight /> } />
+                <Route exact path="/car" element={ <Car /> } />
+                <Route exact path="/vacation" element={ <Vacation /> } />
+                <Route exact path="/package" element={ <Package /> } />
+              </Routes>
+            </div>
+            <div className='bg-gray-300 p-2'>
+              <p className='text-center'>Copyright&copy; Toshiba-Airlines All Rights Reserved { getYear }</p>
+            </div>
+          </>
+        )
+      }
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/screens/Flight.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { RingLoader } from 'react-spinners';
+
+const Flight = () =>
+{
+    const [ navbar, setNavbar ] = useState( false );
+
+    useEffect( () =>
+    {
+        setNavbar( true );
+        setTimeout( () =>
+        {
+            setNavbar( false );
+        }, 1500 );
+    }, [] );
+    return (
+        <>
+            {
+                navbar ? ( <div className="loader"><RingLoader color={ "#FD0155" } loading={ navbar } size={ 150 } /> </div> ) : (
+                    <></>
+                )
+            }
+        </>
+    );
+};
+
+export default Flight;
